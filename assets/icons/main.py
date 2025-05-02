7e265997148f4e2cdcbcc91befa715e79c577bc0
Leo <contractsmart9@gmail.com>
2025-05-03 05:38:11 +0900

    added image

--- assets/icons/main.py
+++ assets/icons/main.py
@@ -1,12 +1,12 @@
 from PIL import Image
 
 # Load the uploaded image
-input_path = "originSp.png"
-output_path = "spain.png"
+input_path = "userb.png"
+output_path = "user.png"
 
 # Open and resize the image
 with Image.open(input_path) as img:
-    resized_img = img.resize((150, 75))
+    resized_img = img.resize((30, 30))
     resized_img.save(output_path)
 
 output_path
